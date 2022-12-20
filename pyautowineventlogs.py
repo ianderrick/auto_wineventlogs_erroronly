dcd2378d9173e867a6446fd298992ac575595b64
Ian Derrick <15677606+ianderrick@users.noreply.github.com>
2022-12-20 08:03:39 -0800

    Update pyautowineventlogs.py

--- pyautowineventlogs.py
+++ pyautowineventlogs.py
@@ -11,16 +11,16 @@ log_names = win32evtlog.GetAllLogNames()
 
 # Open the output file for writing
 with open(output_file_path, "w") as output_file:
-    # Iterate over the log names and read the events from each log
+# Iterate over the log names and read the events from each log
     for log_name in log_names:
         log = win32evtlog.OpenEventLog(log_name)
         flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
         events = win32evtlog.ReadEventLog(log, flags, 0)
 
-        # Iterate over the events and write the errors to the output file
+# Iterate over the events and write the errors to the output file
         for event in events:
             if event.EventType == win32evtlog.EVENTLOG_ERROR_TYPE:
                 output_file.write(str(event))
 
-        # Close the event log
+# Close the event log
         win32evtlog.CloseEventLog(log)
